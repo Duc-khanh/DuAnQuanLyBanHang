93950d6e0c81d4cbfd48a90557258808c2834dc5
Chianh1303 <chianhn567@gmai.com>
2024-10-23 15:49:13 +0700

    chanh update

--- src/main/java/org/example/laptopthachthat/LoginController.java
+++ src/main/java/org/example/laptopthachthat/LoginController.java
@@ -1,10 +1,18 @@
 package org.example.laptopthachthat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,9 +27,6 @@ public class LoginController {
     private Button SigninButton;
     @FXML
     private Label error;
-
-
-
 
     private ConectionJDBC conectionJDBC = new ConectionJDBC();
     @FXML
@@ -54,7 +59,14 @@ public class LoginController {
         return false;
     }
 
-
+    public void switchToDisplayLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(LoginApplication.class.getResource("Register.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Sign up");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     }
 
---
+++ src/main/java/org/example/laptopthachthat/UserController.java
@@ -0,0 +1,4 @@
+package org.example.laptopthachthat;
+
+public class UserController {
+}
