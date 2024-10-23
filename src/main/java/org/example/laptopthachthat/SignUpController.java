b05fc86861766a6e21049853fc68c3b322ce471b
Chianh1303 <chianhn567@gmai.com>
2024-10-23 16:27:42 +0700

    a

--- src/main/java/org/example/laptopthachthat/SignUpController.java
+++ src/main/java/org/example/laptopthachthat/SignUpController.java
@@ -110,4 +110,5 @@ public class SignUpController {
     private boolean ValidatePassword(String password) {
         return password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
     }
+    // haha
 }
