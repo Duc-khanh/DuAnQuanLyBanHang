0ef965387c6ee15cfa4655b39d792268f681c43b
Chianh1303 <chianhn567@gmai.com>
2024-10-28 13:04:28 +0700

    ?

--- src/main/java/org/example/laptopthachthat/LoginController.java
+++ src/main/java/org/example/laptopthachthat/LoginController.java
@@ -1,10 +1,18 @@
 package org.example.laptopthachthat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -52,6 +60,14 @@ public class LoginController {
         }
 
         return false;
+    }
+    public void switchToDisplayRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(LoginApplication.class.getResource("Register.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
     }
 
 
